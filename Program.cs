c9af138a2414283d02b9727c37a75457f11e32e6
eduard <you@example.com>
2022-10-19 20:59:13 +0300

    Решена первая задача

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int [] FillRandomArray(int length, int minVal = -100, int maxVal = 100)
+{
+    int [] arr = new int [length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(minVal,maxVal);
+    }
+    return arr;
+}
+void PrintIntArray(int [] arr)
+{
+    Console.WriteLine();
+    foreach(int i in arr)
+        Console.Write(i + " ");
+    
+}
+////Задача номер 1
+void isEvenArr(int [] arr)
+{
+    int counter = 0;
+    foreach(int i in arr)
+        if(i%2==0)
+            counter++;
+    Console.WriteLine($"В массиве {counter} четных чисел");
+}
+int[] arrNum = FillRandomArray(10,100,1000);
+PrintIntArray(arrNum);
+isEvenArr(arrNum);
